426cce457b34f4c69c265015987c91e1e0cbe7ef
taxilof <taxilof@gmail.com>
2017-02-13 13:11:59 +0100

    changed to python3

--- fetch.py
+++ fetch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
-import cookielib
-import urllib2
+import http.cookiejar
+#import urllib.request
 import urllib
 import json
 
@@ -28,17 +28,27 @@ values = {
 
 
 
-class ChangeTypeProcessor(urllib2.BaseHandler):
+class ChangeTypeProcessor(urllib.request.BaseHandler):
     def http_request(self, req):
         req.unredirected_hdrs["Content-type"] = "application/json;charset=utf-8"
         req.unredirected_hdrs["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
         
         return req
-        
 
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+class Article:
+    code = 0
+    title = ''
+    price_display = ''
+    img_url = ''
+    link = ''
+    def __str__(self):
+        return self.code + " " + self.title
+    
+article_db = []
+    
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 opener.add_handler(ChangeTypeProcessor())
 
 opener.addheaders.append(('Referer', 'http://www.hornbach.de/shop/raus-damit/artikelliste.html?rd=m'))
@@ -51,7 +61,7 @@ opener.addheaders.append(('Referer', 'http://www.hornbach.de/shop/raus-damit/art
 
 
 # WORKING: set market cookie for ulm
-c_hbmark = cookielib.Cookie(0, 'hbMarketCookie', '631', None, False, 'www.hornbach.de', False, False, '/', True, False, 3634071505, False, None, None, None, False)
+c_hbmark = http.cookiejar.Cookie(0, 'hbMarketCookie', '631', None, False, 'www.hornbach.de', False, False, '/', True, False, 3634071505, False, None, None, None, False)
 cj.set_cookie(c_hbmark)
 # show market id:
 # print cj._cookies['www.hornbach.de']['/']['hbMarketCookie'].value
@@ -59,10 +69,10 @@ cj.set_cookie(c_hbmark)
 #print opener.open(url_products).read()
 
 #opener.open(url_hp)
-for i in range(14):
+for i in range(1,2):
     post_data = '{"categoryPath":"","pageNumber":'+str(i)+',"pageSize":500,"sortOrder":"sortModePriceAsc","searchVersion":2,"filters":[]}'
-    response = opener.open(url_products, post_data)
-    products_json = response.read()
+    response = opener.open(url_products, bytearray(post_data,'utf-8'))
+    products_json = response.read().decode('utf8')
     #print products_json
     products_data = json.loads(products_json)
     for article in products_data['articles']:
@@ -72,5 +82,12 @@ for i in range(14):
         img_url = article['imageUrl']
         link = article['localizedExternalArticleLink']
         #print  title + ": " + price + " " + link
-        print "<a target='_blank' href='http://www.hornbach.de/"+link+"?rd=m'><img src='http://www.hornbach.de/"+ img_url+"'> "+price+"E </a><br>"
+        print("<a target='_blank' href='http://www.hornbach.de/"+link+"?rd=m'><img src='http://www.hornbach.de/"+ img_url+"'> "+price+"E </a><br>\r\n")
+        
+        art = Article()
+        art.code = code
+        art.title = title
+        art.img_url = img_url
+        article_db.append(art)
+        
 
