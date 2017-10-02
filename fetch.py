3503bb4cdaeabcb702d83f44e93d0fd8669283dd
Simon Fuchs <simon.fuchs@uni-ulm.de>
2017-10-02 03:29:03 +0200

    switched to https

--- fetch.py
+++ fetch.py
@@ -5,11 +5,11 @@ import urllib
 import json
 from operator import attrgetter
 
-url_hp = "http://www.hornbach.de"
-url_products = "http://www.hornbach.de/mvc/sellout/load-search-results/m.json?_=1486415327126"
+url_hp = "https://www.hornbach.de"
+url_products = "https://www.hornbach.de/mvc/sellout/load-search-results/m.json?_=1506789419698"
 post_data = '{"categoryPath":"","pageNumber":0,"pageSize":500,"sortOrder":"sortModePriceAsc","searchVersion":2,"filters":[]}'
 
-url_market = "http://www.hornbach.de/mvc/market/current-market?_=1486416807490"
+url_market = "https://www.hornbach.de/mvc/market/current-market?_=1486416807490"
 
 
 
@@ -29,7 +29,7 @@ values = {
 
 
 class ChangeTypeProcessor(urllib.request.BaseHandler):
-    def http_request(self, req):
+    def https_request(self, req):
         req.unredirected_hdrs["Content-type"] = "application/json;charset=utf-8"
         req.unredirected_hdrs["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
         
@@ -53,7 +53,7 @@ cj = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 opener.add_handler(ChangeTypeProcessor())
 
-opener.addheaders.append(('Referer', 'http://www.hornbach.de/shop/raus-damit/artikelliste.html?rd=m'))
+opener.addheaders.append(('Referer', 'https://www.hornbach.de/shop/raus-damit/artikelliste.html?rd=m'))
 
 # Cookie(version, name, value, port, port_specified, domain,
 # domain_specified, domain_initial_dot, path, path_specified,
@@ -63,18 +63,20 @@ opener.addheaders.append(('Referer', 'http://www.hornbach.de/shop/raus-damit/art
 
 
 # WORKING: set market cookie for ulm
-c_hbmark = http.cookiejar.Cookie(0, 'hbMarketCookie', '631', None, False, 'www.hornbach.de', False, False, '/', True, False, 3634071505, False, None, None, None, False)
+c_hbmark = http.cookiejar.Cookie(0, 'hbMarketCookie', '631', None, False, 'www.hornbach.de', False, False, '/', True, True, 3634071505, False, None, None, None, False)
 cj.set_cookie(c_hbmark)
 # show market id:
-# print cj._cookies['www.hornbach.de']['/']['hbMarketCookie'].value
+#print (cj._cookies['www.hornbach.de']['/']['hbMarketCookie'])
 
 #print opener.open(url_products).read()
 
-#opener.open(url_hp)
 title_old = ''
 for i in range(1,14):
-    post_data = '{"categoryPath":"","pageNumber":'+str(i)+',"pageSize":500,"sortOrder":"sortModePriceAsc","searchVersion":2,"filters":[]}'
-    response = opener.open(url_products, bytearray(post_data,'utf-8'))
+    post_data = '{"categoryPath":"","pageNumber":'+str(i)+',"pageSize":72,"sortOrder":"sortModePriceAsc","searchVersion":2,"filters":[]}'
+    try:
+        response = opener.open(url_products, bytearray(post_data,'utf-8'))
+    except urllib.error.HTTPError as er:
+        print(er.hdrs)
     products_json = response.read().decode('utf8')
     #print products_json
     products_data = json.loads(products_json)
